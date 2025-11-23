55037ad3bac904b8622e7d885a75366ebee805b2
Niels Rahaga <niels.rahaga-hagamanjaka@eleves.enpc.fr>
2025-11-23 18:32:12 +0100

    jsp

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.firstponthub',
   appName: 'PontHub',
-  webDir: 'dist'
+  webDir: 'build'
 };
 
 export default config;
